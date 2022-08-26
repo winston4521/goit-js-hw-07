a37efaf5346365cdc9b3905d36e77eb52bfa08b5
Igor Nedoshytko <46812129+winston4521@users.noreply.github.com>
2022-08-26 10:54:49 +0200

    ss

---
+++ js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+const imagesMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', imagesMarkup);
+
+galleryContainer.addEventListener('click', onImageClick);
+
+// Create and map elements
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item"
+    <a class="gallery__link"
+        href="${original}">
+        <img class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}">
+       </a>
+</div>`;
+    })
+    .join('');
+}
+
+// Create delegation and biggest element
+
+function onImageClick(e) {
+  e.preventDefault();
+  const isImageEl = e.target.classList.contains('gallery__image');
+
+  if (!isImageEl) {
+    return;
+  }
+  const originalImg = e.target.dataset.source;
+  console.log(originalImg);
+
+  const instance = basicLightbox.create(`
+      <img src="${originalImg}" width="800" height="600">
+  `);
+
+  instance.show();
+
+  // ===========modal close================
+  addEventListener('keydown', onModalClose);
+
+  function onModalClose(e) {
+    if (e.code === 'Escape') {
+      instance.close();
+      removeEventListener('keydown', onModalClose);
+    }
+  }
+}
